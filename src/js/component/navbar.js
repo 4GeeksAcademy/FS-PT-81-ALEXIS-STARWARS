194edb0a9e717b795c92b3f44f1c031a1abd11fc
Cadivals <4geeksalex@gmail.com>
2024-12-08 20:13:54 +0000

    feat:trash_icon

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -10,18 +10,21 @@ export const Navbar = () => {
 				<span className="  navbar-brand mb-0 h1" style={{fontFamily:"Astral Delight"}}>Star Wars</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
+				
 				<div className="dropdown">
 			<button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
 				Favorites
 			</button>
 			<ul className="dropdown-menu mx-5">
+				<i className="fa-solid fa-trash" />
 			
 				</ul>
 				</div>
-			</Link>
+			
 			</div>
 			</div>
+			
 		</nav>
+		
 	);
 };
